b414772fdebb2baaf29cf4f93ebcdf53b550da3e
serenkapanoglu <70541836+serenkapanoglu@users.noreply.github.com>
2023-12-19 17:47:29 -0500

    Update ProductCard.js

--- frontend/src/product/ProductCard.js
+++ frontend/src/product/ProductCard.js
@@ -4,11 +4,34 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import "./ProductCard.css"
 
-const ProductCard = ({ id, name, imgUrl }) => {
+const ProductCard = ({ id, name, imgUrl,price }) => {
   const handleAddCart = (e) => {
+    console.log('clicking to the cart')
     e.preventDefault();
-    console.log('clicked to the add to cart')
+    console.log('clicked to the add to cart');
+    const existingCartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    const newCartItem = {
+      productId: id,
+      name: name,
+      image: imgUrl,
+      price: price,
+      quantity:1
+    };
+    let itemAlreadyInCart = false;
+    const updatedCartItems = existingCartItems.map(item => {
+      if (item.productId === newCartItem.productId) {
+        item.quantity += newCartItem.quantity;
+        itemAlreadyInCart = true;
+      }
+      return item;
+    });
+    if (!itemAlreadyInCart) {
+      updatedCartItems.push(newCartItem);
+    }
+  
+    localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
   }
+ 
   return (
     <Card className="my-3 p-3 rounded" style={{ width: '18rem'}}>
      <Link className='cardtitle' to={`/products/${id}`}>
